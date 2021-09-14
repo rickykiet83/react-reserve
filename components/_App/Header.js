92dbcf366eda50c42c6038aaf2c5b10c5ca8ad4c
Kiet Pham <kietpham.dev@gmail.com>
2021-09-14 21:03:44 +1000

    Create App Layout, Build Header

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,5 +1,73 @@
+import { Container, Icon, Image, Menu } from 'semantic-ui-react';
+
+import Link from 'next/link';
+
 function Header() {
-  return <>Header</>;
+  const user = false;
+  return (
+    <Menu id='menu' inverted>
+      <Container text>
+        <Link href='/'>
+          <Menu.Item header>
+            <Image
+              style={{ marginRight: '1em' }}
+              size='mini'
+              src='/static/logo.svg'
+            ></Image>
+            ReactReserve
+          </Menu.Item>
+        </Link>
+
+        <Link href='/cart'>
+          <Menu.Item header>
+            <Icon name='cart' size='large' />
+            Cart
+          </Menu.Item>
+        </Link>
+        {user && (
+          <Link href='/cart'>
+            <Menu.Item header>
+              <Icon name='plus square' size='large' />
+              Create
+            </Menu.Item>
+          </Link>
+        )}
+
+        {user ? (
+          <>
+            <Link href='/account'>
+              <Menu.Item header>
+                <Icon name='user' size='large' />
+                Account
+              </Menu.Item>
+            </Link>
+
+            <Link href='/logout'>
+              <Menu.Item header>
+                <Icon name='sign out' size='large' />
+                Logout
+              </Menu.Item>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link href='/login'>
+              <Menu.Item header>
+                <Icon name='sign in' size='large' />
+                Login
+              </Menu.Item>
+            </Link>
+            <Link href='/signup'>
+              <Menu.Item header>
+                <Icon name='signup' size='large' />
+                Signup
+              </Menu.Item>
+            </Link>
+          </>
+        )}
+      </Container>
+    </Menu>
+  );
 }
 
 export default Header;
--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -1,8 +1,7 @@
-import Head from "next/head";
-import { Container } from "semantic-ui-react";
-
-import Header from "./Header";
-import HeadContent from "./HeadContent";
+import { Container } from 'semantic-ui-react';
+import Head from 'next/head';
+import HeadContent from './HeadContent';
+import Header from './Header';
 
 function Layout({ children }) {
   return (
@@ -10,16 +9,16 @@ function Layout({ children }) {
       <Head>
         <HeadContent />
         {/* Stylesheets */}
-        <link rel="stylesheet" type="text/css" href="/static/styles.css" />
-        <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+        <link rel='stylesheet' type='text/css' href='/static/styles.css' />
+        <link rel='stylesheet' type='text/css' href='/static/nprogress.css' />
         <link
-          rel="stylesheet"
-          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
+          rel='stylesheet'
+          href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/semantic.min.css'
         />
         <title>ReactReserve</title>
       </Head>
       <Header />
-      <Container text style={{ paddingTop: "1em" }}>
+      <Container text style={{ paddingTop: '1em' }}>
         {children}
       </Container>
     </>
